c26d0a6e07c27d38d514b266706ac336ededf74e
HDU_cjs <kewen324014@outlook.com>
2022-11-24 23:52:42 +0800

    new commit InformationManage.java,2nd edit ProxyInformation.java

---
+++ src/InformationManage.java
@@ -0,0 +1,6 @@
+public interface InformationManage {
+    //模拟用户编辑个人信息
+    void setInformation(String[] information);
+
+    void showInformation();
+}
--- src/ProxyInformation.java
+++ src/ProxyInformation.java
@@ -5,35 +5,95 @@
 
 import java.util.ArrayList;
 
-public class ProxyInformation {
+public class ProxyInformation implements InformationManage{
+    private User realUser;
+    private String userId;
+
+
+    public ProxyInformation(String userId) {
+        this.userId = userId;
+    }
+
+    //代理类查看用户信息
+    @Override
+    public void showInformation() {
+        if (realUser == null) {
+            realUser = new User(userId);
+        }
+        realUser.showInformation();
+    }
+
+    //代理类设置用户信息
+    @Override
+    public void setInformation(String[] information) {
+        String[] informationOld = realUser.getInformation();
+        int changeNum = 0;
+        boolean checkLegal = false;
+
+        if (realUser == null) {
+            realUser = new User(userId);
+        }
+
+        System.out.println("正在检查信息合法性……");
+
+        for(int i = 0; i < information.length; i++) {
+            if(information[i] == null || information[i].replace(" ","").equals("")) {
+                information[i] = informationOld[i];
+            }
+            if(!information[i].equals(informationOld[i])) {
+                changeNum += 1;
+                System.out.printf("change%d: %s  -->  %s%n",changeNum,informationOld[i],information[i]);
+            }
+        }
+
+        if (checkLegal) {
+            System.out.println("变更中存在非法信息，检查不通过！");
+        } else {
+            realUser.setInformation(information);
+            realUser.submitEdit();
+            System.out.println("信息更改成功！");
+        }
+    }
 }
 
-class User {
+class User implements InformationManage{
     private String id;
-    public String name;
-    public String phone;
-    public String dingtalkNumber;
-    public String workStatue;
-    public String male;
-    public String birthday;
-    public String address;
-    public ArrayList<GroupInformation> group;
+    private String name;
+    private String phone;
+    private String dingtalkNumber;
+    private String workStatue;
+    private String male;
+    private String birthday;
+    private String address;
+    private ArrayList<GroupInformation> group;
 
-
-    public void User(String userId){
+    public User(String userId){
         this.id = userId;
         group = new ArrayList<>();
 
-        getInformation();
+        getInformationFromServer();
     }
-    private boolean setInformation() {
-        return true;
+
+    //模拟用户编辑个人信息
+    @Override
+    public void setInformation(String[] information) {
+        this.name = information[0];
+        this.phone = information[1];
+        this.dingtalkNumber = information[2];
+        this.workStatue = information[3];
+        this.male = information[4];
+        this.birthday = information[5];
+        this.address = information[6];
+    }
+
+    public void submitEdit() {
+        System.out.println("正在向数据库提交更改数据……");
     }
 
 
     //模拟从数据库中获取用户信息数据
-    private void getInformation() {
-        System.out.printf("正在从服务器获取ID为：%s的用户信息%n", this.id);
+    private void getInformationFromServer() {
+        System.out.printf("正在从服务器获取ID为：%s的用户信息……%n", this.id);
         this.name = "小明";
         this.phone = "15387590567";
         this.dingtalkNumber = "HDU_xiaomin";
@@ -51,6 +111,7 @@ class User {
     }
 
     //模拟用户查看信息
+    @Override
     public void showInformation() {
         System.out.println("您的个人信息：");
         System.out.println("**************************");
@@ -74,8 +135,25 @@ class User {
             System.out.printf("工号： %s%n", this.group.get(i).workNumber);
         }
     }
+
+    //个人信息返回函数
+    public String[] getInformation() {
+        String[] information = new String[7];
+
+        information[0] = this.name;
+        information[1] = this.male;
+        information[2] = this.birthday;
+        information[3] = this.phone;
+        information[4] = this.dingtalkNumber;
+        information[5] = this.workStatue;
+        information[6] = this.address;
+
+        return information;
+    }
 }
 
+
+//存储用户所在组织信息
 class GroupInformation {
     public String corporationName;
     public String workName;
