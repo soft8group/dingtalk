8208e4c4b56bbfba4af670e6d1fc7a3e84a6cd67
“918617254@qq.com” <918617254@qq.com>
2022-12-04 16:12:34 +0800

    ceshi

---
+++ src/worktodo/CeShi.java
@@ -0,0 +1,33 @@
+package worktodo;
+import java.util.Scanner;
+public class CeShi {
+    public static void main(String[] args) {
+        WorkList workList = new WorkList();
+        Scanner input = new Scanner(System.in);
+        loop:while( true ) {
+            System.out.println("------待办工作界面-------");
+            System.out.println("1.查询工作");
+            System.out.println("2.添加工作");
+            System.out.println("3.删除工作");
+            System.out.println("4.退出");
+            System.out.println("------输入您的选项--------");
+            String str1 = input.nextLine();
+            switch (str1) {
+                case "1":
+                    workList.showWorks();
+                    break;
+                case "2":
+                    workList.addWork();
+                    break;
+                case "3":
+                    workList.deleteWorks();
+                    break;
+                case "4":
+                    break loop;
+                default:
+                    System.out.println("输入错误");
+                    break;
+            }
+        }
+    }
+}
