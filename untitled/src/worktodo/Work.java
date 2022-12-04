669356f536df5c407498930a2042b771d1c6f835
“918617254@qq.com” <918617254@qq.com>
2022-12-04 16:13:58 +0800

    ceshi

---
+++ untitled/src/worktodo/Work.java
@@ -0,0 +1,20 @@
+package worktodo;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class Work {
+    String name;
+    String content;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ untitled/src/worktodo/WorkList.java
@@ -0,0 +1,46 @@
+package worktodo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class WorkList {
+    private List<Work> works = new ArrayList<Work>();
+
+    public void addWork() {
+        Work work = new Work();
+        System.out.println("请输入工作名字：");
+        Scanner input = new Scanner(System.in);
+        if (input.hasNextLine()) {
+            String str1 = input.nextLine();
+            work.setName(str1);
+        }
+        System.out.println("请输入工作内容：");
+        if (input.hasNextLine()) {
+            String str1 = input.nextLine();
+            work.setContent(str1);
+        }
+        works.add(work);
+    }
+
+    public void showWorks() {
+        for (Work work : works) {
+            System.out.println("Work : " + work.getName());
+            System.out.println("Work Content : " + work.getContent());
+        }
+    }
+
+    public void deleteWorks() {
+        System.out.println("请输入要删除的工作：");
+        Scanner input = new Scanner(System.in);
+        if (input.hasNextLine()) {
+            String str1 = input.nextLine();
+            for (Work work : works) {
+                String str2 = work.getName();
+                if (str1.equals(str2)) {
+                    boolean site = works.remove(work);
+                }
+            }
+        }
+    }
+}
