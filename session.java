6659c646a8b8d73dc6dde565a3cb2d06ecaab574
jiahaoran007 <118673834+jiahaoran007@users.noreply.github.com>
2022-12-05 14:33:21 +0800

    接口

---
+++ session.java
@@ -0,0 +1,15 @@
+package compile.java;
+
+public interface session {
+    public void PersonTalk();
+
+    public void GroupTalk();
+
+    // public void message_operate();
+    public interface inner1{
+        public void MessageOperate();
+    }
+
+    public void function();
+}
+
