3defda83f1bf2cfe24eb162244ec7ae1c8d90512
HDU_cjs <kewen324014@outlook.com>
2022-12-07 00:15:07 +0800

    1st edit ProxyMember.java

--- src/com/dingtalk/user/manage/ProxyMember.java
+++ src/com/dingtalk/user/manage/ProxyMember.java
@@ -15,5 +15,20 @@ class Administrator extends User implements MemberManage {
     public void removeMember() {}
 }
 
-class Corporation {
+class  Corporation {
+    private String corporationId;
+    private String corporationName;
+    private static String[] manager = {"adc1","abc2"};
+    private static String[] member = {"adc1","abc2","abc3","abc4"};
+
+    //检查是否为某公司管理员
+    public static boolean isManager(String memberId) {
+        for (String i : manager) {
+            if (i.equals(memberId)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
