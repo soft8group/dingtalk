76cca3ab088f0458a1cfd02305175a62bd879305
DingZhen-HDU <106537035+DingZhen-HDU@users.noreply.github.com>
2022-12-07 23:50:45 +0800

    Delete AbstractSearchFactory.java

--- src/com/dingtalk/user/manage/AbstractSearchFactory.java
+++
@@ -1,6 +0,0 @@
-public abstract class AbstractSearchFactory {
-    abstract public OriginSearchResult getOriginSearch(String origin);
-    abstract public MoreSearchResult getMoreSearch(String more);
-}
-
-
