8c59340f0daa348454b8790f4fd3e1fd4f72c346
jiahaoran007 <118673834+jiahaoran007@users.noreply.github.com>
2022-12-08 00:00:42 +0800

    dev_IM_Talk

---
+++ src/com/dingtalk/user/manage/IM/Talk.java
@@ -0,0 +1,18 @@
+package compile.java;
+
+public class Talk implements session {
+    public void PersonTalk() {
+        System.out.println("创建了一个个人会话");
+    }
+
+    public void GroupTalk() {
+        System.out.println("创建了一个群聊会话");
+    }
+
+    public void function() {
+        System.out.println("为其装饰了一项新功能");
+        
+    }
+
+
+}
