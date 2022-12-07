1755a9443f02ba3a013f76ccb5cbc9375f3d6cea
daguanggun <918617254@qq.com>
2022-12-07 19:44:28 +0800

    kuaihaoleya

--- src/worktodo/WorkListBuild.java
+++ src/worktodo/WorkListBuild.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class WorkListBuild {
     public static void main(String[] args)  {
         WorkList workList = new WorkList();
+        workList.initwork();
         Scanner input = new Scanner(System.in);
         loop:while( true ) {
-            System.out.println("------待办工作界面-------");
-            System.out.println("1.查询工作");
-            System.out.println("2.添加工作");
-            System.out.println("3.删除工作");
-            System.out.println("4.退出");
-            System.out.println("------输入您的选项--------");
+            System.out.println("------待办工作界面------");
+            System.out.println("|     1.查询工作      |");
+            System.out.println("|     2.添加工作      |");
+            System.out.println("|     3.删除工作      |");
+            System.out.println("|     4.退   出      |");
+            System.out.println("------输入您的选项------");
             String str1 = input.nextLine();
             switch (str1) {
                 case "1":
@@ -23,6 +24,7 @@ public class WorkListBuild {
                     workList.deleteWorks();
                     break;
                 case "4":
+                    System.out.println("你可好歹走了");
                     break loop;
                 default:
                     System.out.println("输入错误");
